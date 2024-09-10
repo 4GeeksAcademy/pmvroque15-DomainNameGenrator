b365312a3840aec31b272afbcb3454cec6eb7696
Pat Roque <169104185+pmvroque15@users.noreply.github.com>
2024-09-10 00:29:18 +0000

    initialized and finalized project

--- src/app.js
+++ src/app.js
@@ -7,5 +7,34 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let articles = ["the", "an", "some", "another", "any"];
+  let adjectives = [
+    "awesome",
+    "cool",
+    "intriguing",
+    "thrilling",
+    "addicting",
+    "fun"
+  ];
+  let nouns = ["website", "blog", "webapp", "spot", "hub"];
+  let tlds = [
+    ".com",
+    ".de",
+    ".org",
+    ".space",
+    ".rocks",
+    ".meow",
+    ".cat",
+    ".dog"
+  ];
+
+  for (let article of articles) {
+    for (let adjective of adjectives) {
+      for (let noun of nouns) {
+        for (let tld of tlds) {
+          console.log(`${article}${adjective}${noun}${tld}`);
+        }
+      }
+    }
+  }
 };
